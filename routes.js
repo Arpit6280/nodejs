89a8ea640a61970c2535744949593631092e638b
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2024-02-03 12:40:57 +0530

    clean up code

---
+++ routes.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/" && method === "GET") {
+    const filePath = path.join(__dirname, "message.txt");
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("data", data);
+      res.write("<html>");
+      res.write(`<body> ${data} </body>`);
+      res.write(
+        "<body> <form action='/' method='POST'><input type='text' name='message' /> <button type='send'>Submit</button>  </form> </body>"
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(parsedBody);
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+};
+
+// module.exports = requestHandler;
+
+module.exports = {
+  handler: requestHandler,
+  someText: "Some code",
+};
